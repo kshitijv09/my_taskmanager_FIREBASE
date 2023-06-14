58e8f69545418e449216a9aa05b2c17bd6050634
kshitijv09 <kshitijv09@gmail.com>
2023-06-14 23:28:20 +0530

    Loader and Nav Component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BouncyBallsLoader } from "react-loaders-kit";
+/* import Typed from "react-typed"; */
+
+function Loader() {
+  const loaderProps = {
+    loading: true,
+    size: 275,
+    duration: 2,
+    colors: ["#99fffe", "#f42e00", "#042549"],
+  };
+
+  return (
+    <div className="loader">
+      <BouncyBallsLoader {...loaderProps} />
+      {/* <Typed
+        className="loader-text"
+        strings={["Loading..."]}
+        typeSpeed={60}
+        backSpeed={0}
+      /> */}
+    </div>
+  );
+}
+
+export default Loader;
